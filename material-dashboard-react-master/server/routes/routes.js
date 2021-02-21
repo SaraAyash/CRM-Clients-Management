c035823f4e71e51ebd54fed1232861a5bd947f66
hanibengozi <hanibengozi@gmail.com>
2021-02-21 12:07:56 +0200

    add handling of some requests

--- material-dashboard-react-master/server/routes/routes.js
+++ material-dashboard-react-master/server/routes/routes.js
@@ -1,13 +1,16 @@
 const express = require('express'),
     login = require('./login.js'),
-    dashboard = require('./dashboard.js'),
     clients = require('./clients')
 var router = express.Router();
 
-
+//login
 router.post('/login', login.login);
+
+//clients
 router.get('/clients', clients.get_clients_list);
+router.put('/clients/:clientID', clients.update_client)
 router.post('/clients', clients.add_client);
-router.get('/clients', clients.get_client_details);
- 
+router.get('/clients/search/:clientName', clients.search_client);
+router.get('/clients/new', clients.get_new_clients)
+
 module.exports = router;
