829defbfc4d955e690ddf4c35e268a0342256d2c
hanibengozi <hanibengozi@gmail.com>
2021-03-04 12:46:19 +0200

    trying to update the login component

--- material-dashboard-react-master/src/index.js
+++ material-dashboard-react-master/src/index.js
@@ -2,31 +2,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Provider } from 'react-redux'
-import store from "redux/Store.js"
-
-// core components
-import Admin from "layouts/Admin.js"; 
-import Login from "layouts/login/Login.js";
+import App from './app'
 
 import "assets/css/material-dashboard-react.css?v=1.9.0";
 
 
 const hist = createBrowserHistory();
 
+
 ReactDOM.render(
-  <Provider store ={store}>
-    <Router history={hist}>
-      <Switch>
-        <Route path="/login" component={Login}/>
-        <Route path="/admin" component={Admin} />
-        <Redirect from="/" to="/login" />
-      </Switch>
-    </Router>
-  </Provider>
-  ,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+
+// ReactDOM.render(
+//   <Provider store ={store}>
+//     <Router history={hist}>
+//       <Switch>
+//         <Route path="/login" component={Login}/>
+//         <Route path="/admin" component={Admin} />
+//         <Redirect from="/" to="/login" />
+//       </Switch>
+//     </Router>
+//   </Provider>
+//   ,
+//   document.getElementById("root")
+// );
--- material-dashboard-react-master/src/layouts/login/Login.js
+++ material-dashboard-react-master/src/layouts/login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Login.css';
 
@@ -6,6 +7,7 @@ import './Login.css';
 //sending a post request to the server with the username and password inserted by the user.
 async function loginUser(credentials) {
   console.log(JSON.stringify(credentials));
+
   return fetch('http://localhost:8080/login', {
     method: 'POST',
     headers: {
@@ -13,43 +15,45 @@ async function loginUser(credentials) {
     },
     body: JSON.stringify(credentials)
   })
-    .then(response => {
-      console.log(response);
-    })
+    .then(data => data.json())
  };
 
-
+ 
+ 
 export default function Login( { setToken } ) {
   
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
+  const history = useHistory();  
 
   //handling the press on the submut button.
-  const handleSubmit = async e => {
+  const handleSubmit = async (e, history) => {
     e.preventDefault();
     const token = await loginUser({
       username,
       password
     });
-    //setToken(token);
+    setToken(token);
+    history.push('/admin/dashboard');
   }
+  
   return (
   <div class="cont">
     <div class="demo">
       <div class="login">
         <div class="login__check"></div>
-          <form class="login__form" onSubmit={handleSubmit}>
+          <form class="login__form" onSubmit={e => handleSubmit(e, history)}>
             <div class="login__row">
               <svg class="login__icon name svg-icon" viewBox="0 0 20 20">
                 <path d="M0,20 a10,8 0 0,1 20,0z M10,0 a4,4 0 0,1 0,8 a4,4 0 0,1 0,-8" />
               </svg>
-              <input type="text" class="login__input name"  onChange={e => setUserName(e.target.value)}/>
+              <input type="text" class="login__input name" placeholder="username" onChange={e => setUserName(e.target.value)} required/>
             </div>
             <div class="login__row">
               <svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
                 <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
               </svg>
-              <input type="password" class="login__input pass"  onChange={e => setPassword(e.target.value)}/>
+              <input type="password" class="login__input pass" placeholder="password" onChange={e => setPassword(e.target.value)} required/>
             </div>
             <button type="submit" class="login__submit">Sign in</button>
             <p class="login__signup">Don't have an account? &nbsp;<a>Sign up</a></p>
